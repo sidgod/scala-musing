e0923a2ec98026eb904303ad3a4352a532a99eb5
Sid <siddharth.god@gmail.com>
2013-09-20 12:09:03 +0530

    Updates

--- .worksheet/src/com.sid.scala.pad.workpad.scala
+++ .worksheet/src/com.sid.scala.pad.workpad.scala
@@ -5,5 +5,15 @@ object workpad {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
   def modFunc(x: Int) = if (x >=0) x else -x;System.out.println("""modFunc: (x: Int)Int""");$skip(14); val res$0 = 
   modFunc(-1);System.out.println("""res0: Int = """ + $show(res$0));$skip(13); val res$1 = 
   modFunc(0);System.out.println("""res1: Int = """ + $show(res$1));$skip(13); val res$2 = 
-  modFunc(1);System.out.println("""res2: Int = """ + $show(res$2))}
+  modFunc(1);System.out.println("""res2: Int = """ + $show(res$2));$skip(59); 
+  def and(x: Boolean, y: => Boolean) = if (x) y else false;System.out.println("""and: (x: Boolean, y: => Boolean)Boolean""");$skip(56); 
+	def or(x: Boolean, y: => Boolean) = if (x) true else y;System.out.println("""or: (x: Boolean, y: => Boolean)Boolean""");$skip(17); val res$3 = 
+	and(true, true);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(18); val res$4 = 
+	and(true, false);System.out.println("""res4: Boolean = """ + $show(res$4));$skip(18); val res$5 = 
+	and(false, true);System.out.println("""res5: Boolean = """ + $show(res$5));$skip(19); val res$6 = 
+	and(false, false);System.out.println("""res6: Boolean = """ + $show(res$6));$skip(16); val res$7 = 
+	or(true, true);System.out.println("""res7: Boolean = """ + $show(res$7));$skip(17); val res$8 = 
+	or(true, false);System.out.println("""res8: Boolean = """ + $show(res$8));$skip(17); val res$9 = 
+	or(false, true);System.out.println("""res9: Boolean = """ + $show(res$9));$skip(18); val res$10 = 
+	or(false, false);System.out.println("""res10: Boolean = """ + $show(res$10))}
 }
--- src/com/sid/scala/pad/workpad.sc
+++ src/com/sid/scala/pad/workpad.sc
@@ -6,4 +6,16 @@ object workpad {
   modFunc(-1)                                     //> res0: Int = 1
   modFunc(0)                                      //> res1: Int = 0
   modFunc(1)                                      //> res2: Int = 1
+  def and(x: Boolean, y: => Boolean) = if (x) y else false
+                                                  //> and: (x: Boolean, y: => Boolean)Boolean
+	def or(x: Boolean, y: => Boolean) = if (x) true else y
+                                                  //> or: (x: Boolean, y: => Boolean)Boolean
+	and(true, true)                           //> res3: Boolean = true
+	and(true, false)                          //> res4: Boolean = false
+	and(false, true)                          //> res5: Boolean = false
+	and(false, false)                         //> res6: Boolean = false
+	or(true, true)                            //> res7: Boolean = true
+	or(true, false)                           //> res8: Boolean = true
+	or(false, true)                           //> res9: Boolean = true
+	or(false, false)                          //> res10: Boolean = false
 }
